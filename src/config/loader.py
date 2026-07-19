3fc3ad37e1b3ff3dc7d2d399750c9420b31f36b2
mghitta <mghitta@student.19.be>
2026-07-19 12:52:52 +0200

    feat: models & loader(not finished).

--- src/config/loader.py
+++ src/config/loader.py
@@ -1,37 +1,50 @@
-"""Configure a Level."""
+"""Load the game configuration."""
 
-class LevelConfig:
-    """Represent the configuration of a level."""
-    def __init__(self, width: int, height: int) -> None:
-        """Initialize a level configuration.
+import json
+from typing import Any
+from config.models import Config
 
-        Args:
-            width: width of the level.
-            height: height of the level.
-        """
-        self.width = width
-        self.height = height
 
-class Config:
-    """Represents the game configuration."""
-    def __init__(
-            self,
-            highscore_filename: str,
-            levels: list[LevelConfig],
-            lives: int,
-            pacgum: int,
-            points_per_pacgum: int,
-            points_per_super_pacgum: int,
-            points_per_ghost: int,
-            seed: int,
-            level_max_time: int,
-    ) -> None:
-        self.highscore_filename = highscore_filename
-        self.levels = levels
-        self.lives = lives
-        self.pacgum = pacgum
-        self.points_per_pacgum = points_per_pacgum
-        self.points_per_ghost = points_per_ghost
-        self.seed = seed
-        self.level_max_time = level_max_time
-        self.points_per_super_pacgum = points_per_super_pacgum
+class ConfigLoader:
+    """Load the game configuration."""
+
+    def _remove_comments(self, content: str) -> str:
+        clean_lines = []
+        for line in content.splitlines():
+            if (line.strip().startswith("#")
+               or line.strip().startswith("//")):
+                continue
+            clean_lines.append(line)
+        return "\n".join(clean_lines)
+
+    def _build_config(self, data: dict[str, Any]) -> Config:
+        highscore_filename = data.get("highscore_filename",
+                                      "highscores.json")
+        lives = data.get("lives", 3)
+        pacgum = data.get("pacgum", 42)
+        points_per_pacgum = data.get("points_per_pacgum", 10)
+        points_per_super_pacgum = data.get("points_per_super_pacgum", 50)
+        points_per_ghost = data.get("points_per_ghost", 200)
+        seed = data.get("seed", 42)
+        level_max_time = data.get("level_max_time", 90)
+        levels = []
+        return Config(
+            highscore_filename=highscore_filename,
+            levels=levels,
+            lives=lives,
+            pacgum=pacgum,
+            points_per_pacgum=points_per_pacgum,
+            points_per_super_pacgum=points_per_super_pacgum,
+            points_per_ghost=points_per_ghost,
+            seed=seed,
+            level_max_time=level_max_time
+        )
+
+    def load(self, path: str) -> Config:
+        """Load the configuration from a file."""
+
+        with open(path, "r", encoding="utf-8") as file:
+            content = file.read()
+            content = self._remove_comments(content)
+            data = json.loads(content)
+        return self._build_config(data)
