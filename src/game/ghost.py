58697e082e5ab3664e2bb15e0bd86b5475b6058a
Julien Pik <jpik@fu-r3-p3.s19.be>
2026-08-13 14:13:07 +0200

    fix: changed distance calculus from Manhattan distance to Euclidean distance

--- src/game/ghost.py
+++ src/game/ghost.py
@@ -112,7 +112,7 @@ class Ghost:
         best_distance = float("inf")
         for direction in directions:
             next_x, next_y = self._next_position(direction)
-            distance = abs(target_x - next_x) + abs(target_y - next_y)
+            distance = math.hypot(target_x - next_x, target_y - next_y)
             if distance < best_distance:
                 best_distance = distance
                 best_directions = [direction]
