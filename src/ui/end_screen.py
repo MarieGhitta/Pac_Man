4cd5b5056c219f422d940dcb84c28ec544a9b5c2
Julien Pik <jpik@fu-r3-p3.s19.be>
2026-08-24 14:16:35 +0200

    feat: get username input then access end screen menu

--- src/ui/end_screen.py
+++ src/ui/end_screen.py
@@ -23,27 +23,51 @@ class EndScreen(Screen):
         self.overlay_alpha: int = 0
         self.logo_alpha: int = 0
         self.menu_alpha: int = 0
+        self.score_alpha: int = 0
         self.menu_items: list[str] = [
             "Retry", "Main Menu", "Quit"
         ]
         self.can_navigate: bool = False
+        self.username: str = ""
+        self.score_input: list[str] = [
+            "Enter your name:", self.username
+        ]
+        self.can_write: bool = False
 
     def handle_event(self, event: pygame.event.Event) -> None:
-        if self.can_navigate:
-            if event.type == pygame.KEYDOWN:
-                self._navigate(event.key)
+        if event.type == pygame.KEYDOWN:
+            if self.can_write:
                 match event.key:
+                    case pygame.K_BACKSPACE:
+                        self.username = self.username[:-1]
                     case pygame.K_RETURN:
-                        match self.menu_index:
-                            case 0:
-                                self.next_screen = "game"
-                            case 1:
-                                self.next_screen = "title"
-                            case 2:
-                                self.next_screen = "quit"
-                    case pygame.K_ESCAPE:
-                        self.next_screen = "quit"
-            self.can_navigate = False
+                        if len(self.username) > 2:
+                            self.can_write = False
+                            self.can_navigate = True
+                    case _:
+                        if (
+                            event.unicode.isalnum()
+                            and len(self.username) < 10
+                        ):
+                            self.username += event.unicode
+                if len(self.username) == 10 and self.can_write:
+                    self.can_write = False
+            else:
+                if self.can_navigate:
+                    self._navigate(event.key)
+                    match event.key:
+                        case pygame.K_RETURN:
+                            match self.menu_index:
+                                case 0:
+                                    self.next_screen = "game"
+                                case 1:
+                                    self.next_screen = "title"
+                                case 2:
+                                    self.next_screen = "quit"
+                        case pygame.K_ESCAPE:
+                            self.next_screen = "quit"
+                    if self.next_screen is not None:
+                        self.can_navigate = False
 
     def update(self, current_time: int) -> None:
         elapsed = current_time - self.fade_start
@@ -55,10 +79,17 @@ class EndScreen(Screen):
             fade_out_progress = min(1.0, (elapsed - 3500) / 800)
             self.logo_alpha = int((1.0 - fade_out_progress) * 255)
             if elapsed > 3500:
-                menu_progress = max(0.0, (elapsed - 4300) / 1000)
-                self.menu_alpha = int(min(1.0, menu_progress) * 255)
+                score_progress = max(0.0, (elapsed - 4300) / 1000)
+                self.score_alpha = int(min(1.0, score_progress) * 255)
                 if elapsed > 5000:
-                    self.can_navigate = True
+                    if not self.can_navigate:
+                        self.can_write = True
+                    else:
+                        self.score_alpha = 0
+                        menu_progress = max(0.0, (elapsed - 1000) / 1000)
+                        self.menu_alpha = int(min(1.0, menu_progress) * 255)
+
+
 
     def draw(self) -> None:
         self.overlay.fill((0, 0, 0, self.overlay_alpha))
@@ -70,7 +101,32 @@ class EndScreen(Screen):
             (2, 2),
             self.logo_alpha
         )
+
+        # input_txt = self.font.render("Enter your name:", True, Color.WHITE) 
+        # input_txt_rect = input_txt.get_rect(
+        #     center=(self.width // 2, self.height // 2)
+        # )
+        # self.surface.blit(input_txt, input_txt_rect)
+
+        line_height = int(self.font.get_height() * 1.5)
+        total_height = len(self.score_input) * line_height
+        menu_start_y = self.height // 2 - total_height // 2
+        self.score_input[-1] = self.username
+        self._draw_menu(
+            self.score_input,
+            self.font,
+            line_height,
+            menu_start_y,
+            self.score_alpha
+        )
+
         line_height = int(self.font.get_height() * 1.5)
         total_height = len(self.menu_items) * line_height
         menu_start_y = self.height * 3 // 4 - total_height // 2
-        self._draw_menu(self.font, line_height, menu_start_y, self.menu_alpha)
+        self._draw_menu(
+            self.menu_items,
+            self.font,
+            line_height,
+            menu_start_y,
+            self.menu_alpha
+        )
